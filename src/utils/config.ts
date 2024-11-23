62e9fbce05fc72cca61a3350d655e720c5ad5446
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2024-11-23 18:18:30 +0100

    url fix

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,2 +1,2 @@
-export const DB_URI = process.env.DB_URI || "https://admin:admin@localhost:5984";
+export const DB_URI = process.env.DB_URI || "http://admin:admin@localhost:5984";
 export const PORT = process.env.PORT || 3000;
