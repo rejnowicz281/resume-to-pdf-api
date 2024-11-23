b71012356d5d76c153fce363ff7400a2ceafdb67
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2024-11-23 13:23:21 +0100

    Logging

--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,21 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import config from "config";
 import express from "express";
 import routes from "./routes";
 import logger from "./utils/logger";
 import middleware from "./utils/middleware";
 
 const port = process.env.PORT || 3000;
+const dbUri = config.get("dbUri");
 
 const app = express();
 
 middleware(app);
 
 app.listen(port, async () => {
-    logger.info(`Server running on port ${port}`);
+    logger.info(`Server running on port ${port}, connecting to ${dbUri}`);
 
     routes(app);
 });
